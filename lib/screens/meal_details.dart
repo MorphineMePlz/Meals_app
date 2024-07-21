1ed510560803893b13e68267d3e6f2f550a4a428
unknown <aziz706797@gmail.com>
2024-07-21 12:27:44 +0500

    add animation between screens

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -22,27 +22,39 @@ class MealDetailsScreen extends ConsumerWidget {
           title: Text(meal.title),
           actions: [
             IconButton(
-                onPressed: () {
-                  final wasAdded = ref
-                      .read(favoriteMealProvider.notifier)
-                      .toggleMealFavoriteStatus(meal);
-                  ScaffoldMessenger.of(context).clearSnackBars();
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(wasAdded
-                          ? 'Meal added as a favorite'
-                          : 'Meal Removed')));
-                },
-                icon: Icon(isFavorite ? Icons.star : Icons.star_border))
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealProvider.notifier)
+                    .toggleMealFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(wasAdded
+                        ? 'Meal added as a favorite'
+                        : 'Meal Removed')));
+              },
+              icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                      turns: Tween<double>(begin: 0.1, end: 1).animate(animation),
+                      child: child,
+                    );
+                  },
+                  child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),)),
+            )
           ],
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 300,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                ),
               ),
               const SizedBox(
                 height: 14,
